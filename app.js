2c67318ce7be785688d9aa54d3d679fc375c3516
Abishek Haththakage <aloka.abishek9@gmail.com>
2024-12-08 11:38:59 +0530

    Refactor MongoDB connection error handling and improve code formatting

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // MongoDB Connection with enhanced error handling
 const mongoURI = process.env.MONGO_URI;
 if (!mongoURI) {
-  console.error('MongoDB URI is not defined in the environment variables.');
-  process.exit(1); // Exit the process if MongoDB URI is not set
+    console.error('MongoDB URI is not defined in the environment variables.');
+    process.exit(1); // Exit the process if MongoDB URI is not set
 }
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => {
-    console.error('MongoDB connection error: ', err);
-    process.exit(1); // Exit the process if connection fails
-  });
+mongoose.connect(mongoURI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => {
+        console.error('MongoDB connection error: ', err);
+        process.exit(1); // Exit the process if connection fails
+    });
 
 // Routes
 app.use('/history', historyRoutes);
@@ -41,13 +41,14 @@ app.use('/food', foodRoutes);
 
 // Error handling for undefined routes
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route not found' });
+    res.status(404).json({ message: 'Route not found' });
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
 });
+
 
 module.exports = app;
